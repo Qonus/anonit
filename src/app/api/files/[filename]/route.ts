3fb44a111d9faaca4b5d045c1296aaa8e589910f
Qonus <a1bat111k@mail.ru>
2025-05-18 12:17:47 +0500

    Update route.ts

--- src/app/api/files/[filename]/route.ts
+++ src/app/api/files/[filename]/route.ts
@@ -1,9 +1,10 @@
 
 import { readFile } from 'fs/promises';
 import mime from 'mime-types';
+import { NextRequest } from 'next/server';
 import path from 'path';
 
-export async function GET(req: Request, { params }: { params: { filename: string } }) { // eslint-disable-line no-use-before-define
+export async function GET(req: NextRequest, { params }: { params: { filename: string } }) { // eslint-disable-line no-use-before-define
   const filePath = path.join('/tmp', params.filename);
   const contentType = mime.lookup(filePath) || 'application/octet-stream';
   try {
@@ -12,6 +13,7 @@ export async function GET(req: Request, { params }: { params: { filename: string
       headers: {
         'Content-Type': contentType,
         'Cache-Control': 'no-store',
+        'Content-Disposition': 'inline',
       },
     });
   } catch (e) {
